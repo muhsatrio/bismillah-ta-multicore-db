191ff2bc26a9a7e6dcbc06965f787a588eb7471b
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-04-08 00:57:35 +0700

    change preprocessing without MPI

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -o run.out main.cpp lib/vertex.cpp lib/vector.cpp lib/output.cpp lib/perpendicular_bisector.cpp lib/point.cpp -l mysqlcppconn
+g++ -o run.out preprocessing.cpp lib/vertex.cpp lib/vector.cpp lib/output.cpp lib/perpendicular_bisector.cpp lib/point.cpp -l mysqlcppconn
 ./run.out
